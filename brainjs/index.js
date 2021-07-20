4a4a1205483bcc192a35c3c7ce833d57e6b17b85
Jerim Kaura <50904889+jerimkaura@users.noreply.github.com>
2021-07-20 11:31:37 +0300

    Update index.js

--- brainjs/index.js
+++ brainjs/index.js
@@ -8,13 +8,13 @@ const network = new brain.NeuralNetwork();
  * Training data
  */
 network.train([
-    { input: [1, 2], output: [1] }, // Team 2 wins
-    { input: [1, 3], output: [1] }, // Team 3 wins
-    { input: [2, 3], output: [0] }, // Team 2 wins
-    { input: [2, 4], output: [1] }, // Team 4 wins
-    { input: [1, 2], output: [0] }, // Team 1 wins
-    { input: [1, 3], output: [0] }, // Team 1 wins
-    { input: [3, 4], output: [0] }  // Team 3 wins
+    { input: [1, 2], output: [1] }, 
+    { input: [1, 3], output: [1] }, 
+    { input: [2, 3], output: [0] }, 
+    { input: [2, 4], output: [1] }, 
+    { input: [1, 2], output: [0] }, 
+    { input: [1, 3], output: [0] }, 
+    { input: [3, 4], output: [0] }  
 ]);
 
 const result = network.run([1, 4]);
